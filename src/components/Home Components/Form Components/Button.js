5830a891b73829080797783fab5d97960ca79e1a
karim Guezlane <k.guezlane@gmail.com>
2021-09-21 21:41:20 +0100

    styled button

--- src/components/Home Components/Form Components/Button.js
+++ src/components/Home Components/Form Components/Button.js
@@ -1,8 +1,13 @@
-import React from "react";
 import styled from "styled-components";
 
-const FormButton = styled.button``;
-export default function Button(props) {
-    const {type}=props;
-	return <FormButton type={type}></FormButton>;
-}
+const FormButton = styled.button`
+	background: 0 0;
+	border: 0;
+	box-sizing: border-box;
+	cursor: pointer;
+	font-weight: 600;
+	padding: 5px 9px;
+	text-align: center;
+	width: auto;
+`;
+export {FormButton}
