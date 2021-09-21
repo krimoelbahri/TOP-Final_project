349e1e165570a19122d1c9ee53ea057ce62d9cd2
karim Guezlane <k.guezlane@gmail.com>
2021-09-21 19:12:03 +0100

    Form component for reusing

---
+++ src/components/Home Components/Form Components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+const FormComponent = styled.form`
+	max-width: 350px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+	border: 1px solid black;
+`;
+export default function Form({ children }) {
+	return <FormComponent>{children}</FormComponent>;
+}
