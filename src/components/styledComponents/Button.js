07fbacd38716dcbd297eeb9c649a56b44e6d050d
karim Guezlane <k.guezlane@gmail.com>
2021-09-22 11:25:58 +0100

    created a styled button

---
+++ src/components/styledComponents/Button.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FormButton = styled.button`
+	background: 0 0;
+	border: 0;
+	box-sizing: border-box;
+	cursor: pointer;
+	font-weight: 600;
+	padding: 5px 9px;
+	text-align: center;
+	width: auto;
+`;
+export { FormButton }
