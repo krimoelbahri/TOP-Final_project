cdbb570cbb0fdfca49d78035253383e368a3bd35
karim Guezlane <k.guezlane@gmail.com>
2021-09-22 18:36:21 +0100

    finishing up login Form

---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import LoginForm from "./Login Components/LoginForm";
+import styled from "styled-components";
+import Container from "./styledComponents/FormConatiner";
+const LoginContainer = styled.div`
+	max-width: 350px;
+	display: flex;
+	flex-direction: column;
+	margin: 50px auto 10px;
+`;
+const StyledLink = styled.a`
+	color: #0095f6;
+	font-size: 14px;
+	font-weight: 600;
+	line-height: 18px;
+`;
+export default function Home() {
+	return (
+		<LoginContainer>
+			<LoginForm />
+			<Container>
+				<div>
+					<p
+						style={{
+							margin: "15px",
+						}}>
+						Don't have an account? <StyledLink href='/'>Sign up</StyledLink>
+					</p>
+				</div>
+			</Container>
+		</LoginContainer>
+	);
+}
