8bc555e67a247739bda8f117eb2f7c71cdfeac78
karim Guezlane <k.guezlane@gmail.com>
2021-09-22 12:05:48 +0100

    handling enabling and disabeling submit button

--- src/components/Home Components/Form Components/FormInput.js
+++ src/components/Home Components/Form Components/FormInput.js
@@ -29,17 +29,19 @@ const Span = styled.span`
 	text-overflow: ellipsis;
 `;
 export default function FormInput(props) {
-	const { ariaLabel, type, inputID ,children} = props;
+	const { ariaLabel, inputRef, type, inputID,onChange, children } = props;
 	const [inputValue, setinputValue] = useState();
 	return (
 		<>
 			<Span animation={inputValue ? true : false}>{ariaLabel}</Span>
 			<Input
+				ref={inputRef}
 				id={inputID}
 				type={type}
 				animation={inputValue ? true : false}
 				onChange={(e) => {
 					setinputValue(e.target.value);
+					onChange();
 				}}
 				aria-label={ariaLabel}>
 				{children}
