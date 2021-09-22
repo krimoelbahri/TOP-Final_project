b70a40ccf0814f0c4a53df6a084e23cdeb2e8b4e
karim Guezlane <k.guezlane@gmail.com>
2021-09-22 18:35:33 +0100

    creating a container for the Form

---
+++ src/components/styledComponents/FormConatiner.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const FormConatiner = styled.div`
+	max-width: 350px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+	border: 1px solid #dbdbdb;
+	padding: 10px 0;
+	margin-bottom:10px;
+	background-color:white;
+
+`;
+export default FormConatiner;
