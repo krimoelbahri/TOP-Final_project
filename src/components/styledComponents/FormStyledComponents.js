3cce807fbf6d34b92f3b1a779dd4ce99fb59a4f0
karim Guezlane <k.guezlane@gmail.com>
2021-09-23 17:25:36 +0100

    added some general styled Form components

---
+++ src/components/styledComponents/FormStyledComponents.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+const Logo = styled.h1`
+	margin: 30px 0 30px 0;
+`;
+const FormDiv = styled.div`
+	margin: 0 40px 6px;
+	width: 75%;
+`;
+const Label = styled.label`
+	display: flex;
+	height: 36px;
+	flex: 1 0 0;
+	padding: 0;
+	position: relative;
+	margin: 0;
+	min-width: 0;
+`;
+export {Logo,FormDiv,Label}
