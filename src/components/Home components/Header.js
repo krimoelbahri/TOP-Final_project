16346d1b5deeed43bca3b673f9d62f9b20f5ffa7
karim Guezlane <k.guezlane@gmail.com>
2021-09-24 17:05:37 +0100

    added components to Header

--- src/components/Home components/Header.js
+++ src/components/Home components/Header.js
@@ -25,3 +25,39 @@ const Icon = styled.div`
 	margin-left: 20px;
 `;
 
+export default function Header() {
+	return (
+		<Container>
+			<HeaderDiv>
+				<Wraper justifyContent='flex-start' className='header-logo'>
+					Fakegram
+				</Wraper>
+				<Wraper justifyContent='center' className='header-logo'>
+					<input type='text' placeholder='search'></input>
+				</Wraper>
+				<Wraper justifyContent='flex-end' className='header-logo'>
+					<div>
+						<Icon>
+							<i className='bi bi-house-door'></i>
+						</Icon>
+						<Icon>
+							<i className='bi bi-chat'></i>
+						</Icon>
+						<Icon>
+							<i className='far fa-compass'></i>
+						</Icon>
+						<Icon>
+							<i className='bi bi-heart'></i>
+						</Icon>
+
+						<Icon
+							style={{
+								border: "1px solid black",
+								borderRadius: "50%",
+							}}></Icon>
+					</div>
+				</Wraper>
+			</HeaderDiv>
+		</Container>
+	);
+}
