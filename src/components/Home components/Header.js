2716799c1729ab16d546b01d19c6c479fab04db4
karim Guezlane <k.guezlane@gmail.com>
2021-09-24 17:03:30 +0100

    created Container for Header comonent

---
+++ src/components/Home components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderDiv from "../styledComponents/HederDiv";
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	min-width: 100%;
+	height: 55px;
+	border-bottom: 1px solid #dbdbdb;
+	background-color: white;
+	position: fixed;
+	top: 0;
+`;
+
