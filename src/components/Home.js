40928b99ff78ab8c6bba0c8c3011b9e2a6f85608
karim Guezlane <k.guezlane@gmail.com>
2021-09-24 17:09:11 +0100

    added Header to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Header from "./Home components/Header";
+const Container = styled.div`
+	min-width: 100%;
+	overflow:hidden;
+
+`;
 export default function Home() {
-    return (
-        <div>
-        </div>
-    )
+	return (
+		<Container>
+			<Header />
+		</Container>
+	);
 }
