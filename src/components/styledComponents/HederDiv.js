bf5afb7700963b96acb1d27b6a57641885157626
karim Guezlane <k.guezlane@gmail.com>
2021-09-24 17:07:08 +0100

    a styled Div to wrap Header sub components

---
+++ src/components/styledComponents/HederDiv.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HeaderDiv = styled.div`
+	max-width: 950px;
+    height:55px;
+    align-items: center;
+	display: flex;
+	flex-direction: row;
+	padding: 0 15px;
+`;
+export default HeaderDiv;
