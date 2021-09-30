cca7b587303c6e63816e4cc1d8ed1b8466618857
karim Guezlane <k.guezlane@gmail.com>
2021-09-30 10:30:35 +0100

    created main component to show posts on

---
+++ src/components/Home components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import PostComponent from "./PostComponent";
+
+const Container = styled.div``;
+export default function Main() {
+	return (
+		<Container>
+			<PostComponent />
+		</Container>
+	);
+}
