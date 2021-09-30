7b3a4b6fcde4b4f101b14743f4431340d31993a9
karim Guezlane <k.guezlane@gmail.com>
2021-09-30 18:48:33 +0100

    finishing up the post image component

--- src/components/Home components/PostImage.js
+++ src/components/Home components/PostImage.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import sample from "../../assets/sample-img.jpg";
+const Container = styled.div`
+	width: 100%;
+	min-height: 300px;
+`;
+const Image = styled.img`
+	width: 100%;
+	height: 100%;
+`;
 
 export default function PostImage() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<Container>
+			<Image src={sample} alt='sample-img'></Image>
+		</Container>
+	);
 }
