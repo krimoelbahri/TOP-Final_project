0a1c202606b1caa378536587c87ec8d9d73a60b8
karim Guezlane <k.guezlane@gmail.com>
2021-09-30 11:28:18 +0100

    created a post container div

---
+++ src/components/Home components/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	width: 100%;
+	height: inherit;
+	border: 1px solid #dbdbdb;
+	border-radius: 3px;
+	margin: 50px 0 20px 0;
+`;
+
+export default function Post() {
+	return <Container></Container>;
+}
