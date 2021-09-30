8dada137d9de31d5786902016815e81c54f3e9c1
karim Guezlane <k.guezlane@gmail.com>
2021-09-30 11:53:48 +0100

    adjusted display and margin of posts container

--- src/components/Home components/Post.js
+++ src/components/Home components/Post.js
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import PostHeader from "./PostHeader";
 
 const Container = styled.div`
+	display: flex;
+	flex-direction: column;
 	width: 100%;
 	height: inherit;
 	border: 1px solid #dbdbdb;
 	border-radius: 3px;
-	margin: 50px 0 20px 0;
+	margin: 20px 0 0 0;
 `;
 
 export default function Post() {
-	return <Container></Container>;
+	return (
+		<Container>
+			<PostHeader />
+		</Container>
+	);
 }
