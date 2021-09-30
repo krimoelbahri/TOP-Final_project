aec472f211e65ecff0dabb7f039e4ae560060d6c
karim Guezlane <k.guezlane@gmail.com>
2021-09-30 11:52:40 +0100

    created a Container for the Post header

--- src/components/Home components/PostHeader.js
+++ src/components/Home components/PostHeader.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+	width: 100%;
+	height: 50px;
+	display: flex;
+	flex-direction: row;
+    justify-content: space-between;
+`;
 
 export default function PostHeader() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<Container>
+			<div></div>
+			<div></div>
+		</Container>
+	);
 }
