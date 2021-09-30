549aae0af30d2190605bdcf475f691e74ccdcaee
karim Guezlane <k.guezlane@gmail.com>
2021-09-30 11:53:08 +0100

    added padding to main container

--- src/components/Home components/Main.js
+++ src/components/Home components/Main.js
@@ -5,6 +5,7 @@ import Post from "./Post";
 const Container = styled.div`
 	max-width: 600px;
 	position: relative;
+	padding-top: 60px;
 	top: 55px;
 	margin: 0 auto 100px;
 `;
@@ -13,7 +14,7 @@ export default function Main() {
 	return (
 		<Container>
 			<Post />
-            <Post />
+			<Post />
 			<Post />
 		</Container>
 	);
