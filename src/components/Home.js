56bc6fda7e0ab1603a366fb60e193c7cfda32c4a
karim Guezlane <k.guezlane@gmail.com>
2021-10-01 18:13:29 +0100

    added main component to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "./Home components/Header";
+import Main from "./Home components/Main";
+
 const Container = styled.div`
 	min-width: 100%;
 	overflow: hidden;
@@ -9,6 +11,7 @@ export default function Home() {
 	return (
 		<Container>
 			<Header />
+			<Main />
 		</Container>
 	);
 }
