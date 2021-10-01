1628ece2b57a85d2f4b081b238fbb551a305cee8
karim Guezlane <k.guezlane@gmail.com>
2021-10-01 18:12:46 +0100

    finishing up the comments component

--- src/components/Home components/PostComments.js
+++ src/components/Home components/PostComments.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Comment from "./Comment";
+const Container = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+`;
 
 export default function PostComments() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<Container>
+			<Comment />
+            <Comment />
+            <Comment />
+		</Container>
+	);
 }
