5c4579417e17cb0fe341e8d09008b461c5ea147a
karim Guezlane <k.guezlane@gmail.com>
2021-10-03 11:00:36 +0100

    styled links, and added a form to add comments

--- src/components/Home components/PostComments.js
+++ src/components/Home components/PostComments.js
@@ -1,22 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import AddComment from "./AddComment";
 import Comment from "./Comment";
+
 const Container = styled.div`
 	width: 100%;
 	display: flex;
 	flex-direction: column;
 `;
+const ViewCommentsLink = styled.a`
+	padding-left: 20px;
+	margin-top: 5px;
+	color: #8e8e8e;
+`;
+const AgeLink = styled(ViewCommentsLink)`
+	font-size: 10px;
+	margin-bottom: 15px;
+`;
 
 export default function PostComments() {
 	let commentsNumber = 200;
+	let postAge = "1 DAY";
 	return (
 		<Container>
 			<Comment />
 			<Comment />
 			<Comment />
-			<a href='/'>
+			<ViewCommentsLink href='/'>
 				<span>View all {commentsNumber} comments</span>
-			</a>
+			</ViewCommentsLink>
+			<AgeLink>
+				<span>{postAge} AGO</span>
+			</AgeLink>
+			<AddComment />
 		</Container>
 	);
 }
