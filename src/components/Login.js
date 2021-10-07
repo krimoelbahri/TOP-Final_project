3e8ae55a3f34d8925d92e5c77816573463b00953
karim Guezlane <k.guezlane@gmail.com>
2021-10-07 15:05:49 +0100

    facebook login function

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,29 +2,38 @@ import React from "react";
 import LoginForm from "./Form Components/LoginForm";
 import styled from "styled-components";
 import Container from "./styledComponents/FormConatiner";
+import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+
 const LoginContainer = styled.div`
 	max-width: 350px;
 	display: flex;
 	flex-direction: column;
 	margin: 50px auto 10px;
 `;
-const StyledLink = styled.a`
+const StyledLink = styled(Link)`
 	color: #0095f6;
 	font-size: 14px;
 	font-weight: 600;
 	line-height: 18px;
 `;
 export default function Login() {
+	const { loginWithFacebook } = useAuth();
+	function handleFacebookLogin() {
+		console.log("yes");
+		loginWithFacebook();
+	}
 	return (
 		<LoginContainer>
-			<LoginForm />
+			<LoginForm handleFacebookLogin={handleFacebookLogin} />
 			<Container>
 				<div>
 					<p
 						style={{
 							margin: "15px",
 						}}>
-						Don't have an account? <StyledLink href='/'>Sign up</StyledLink>
+						Don't have an account?{" "}
+						<StyledLink to='/signup'>Sign up</StyledLink>
 					</p>
 				</div>
 			</Container>
