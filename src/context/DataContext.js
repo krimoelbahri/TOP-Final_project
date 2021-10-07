d537e603ad32decc52ea88e0f86b45bb8400797e
karim Guezlane <k.guezlane@gmail.com>
2021-10-07 15:07:18 +0100

    created data context

---
+++ src/context/DataContext.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import {
+	doc,
+	getDoc,
+	getDocs,
+	setDoc,
+	where,
+	orderBy,
+	limit,
+	collection,
+	query,
+} from "firebase/firestore";
+import { db } from "../firebase";
+
+const DataContext = React.createContext();
+
+export function useData() {
+	return useContext(DataContext);
+}
+
+export function DataProvider({ children }) {
+	async function getData(collectionName, document) {
+		const data = await getDoc(doc(db, collectionName, document));
+		return data;
+	}
+	
+	async function setData(collectionName, data) {
+		await setDoc(doc(collection(db, collectionName)), data);
+	}
+
+	const value = { getData, setData };
+
+	return (
+		<DataContext.Provider value={value}>
+			{children}
+		</DataContext.Provider>
+	);
+}
