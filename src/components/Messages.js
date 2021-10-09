7ae08d106bb74d474e9354ebb5e1b4e47f2c973b
karim Guezlane <k.guezlane@gmail.com>
2021-10-09 18:07:44 +0100

    created a messages page

---
+++ src/components/Messages.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Home Components/Header";
+import Main from "./Home Components/Main";
+
+const Container = styled.div`
+	min-width: 100%;
+	overflow: hidden;
+`;
+export default function Messages() {
+	return (
+		<Container>
+			<Header />
+			<Main />
+		</Container>
+	);
+}
