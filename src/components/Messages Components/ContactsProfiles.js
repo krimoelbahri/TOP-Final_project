e4393c923e51842617e711809bce63948f67874e
karim Guezlane <k.guezlane@gmail.com>
2021-10-10 18:08:35 +0100

    styling container for profiles

--- src/components/Messages Components/ContactsProfiles.js
+++ src/components/Messages Components/ContactsProfiles.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+	height: 100%;
+	width: 100%;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: flex-start;
+	padding: 15px 15px 0 0;
+`;
 export default function ContactsProfiles() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return <Container></Container>;
 }
