0cf76e7b169b44b1bd1bd86c3426e11e2a075767
karim Guezlane <k.guezlane@gmail.com>
2021-10-10 17:56:02 +0100

    styled container in Inbox part messagery

--- src/components/Messages Components/Inbox.js
+++ src/components/Messages Components/Inbox.js
@@ -1,5 +1,13 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+	height: 100%;
+	width: 60%;
+	min-width: 350px;
+	display: flex;
+	flex-direction: row;
+`;
 export default function Inbox() {
-	return <div></div>;
+	return <Container></Container>;
 }
