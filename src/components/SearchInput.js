e20241b1df0288e3429a8be88e001842d6ace1f7
karim Guezlane <k.guezlane@gmail.com>
2021-10-14 22:04:43 +0100

    seperating styling from components

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,31 +1,6 @@
 import React, { useState, useRef } from "react";
-import styled from "styled-components";
-const Input = styled.input`
-	background: rgba(250, 250, 250, 1);
-	outline: 0;
-	overflow: hidden;
-	padding: 5px 5px 5px 26px;
-	font-size: 16px;
-	text-overflow: ellipsis;
-	border: 1px solid #dbdbdb;
-	border-radius: 3px;
-	width: -webkit-fill-available;
-`;
-const Span = styled.span`
-	background: rgba(250, 250, 250, 1);
-	color: rgba(142, 142, 142, 1);
-	border: 1px solid #dbdbdb;
-	border-radius: 3px;
-	padding: 7px;
-	font-family: sans-serif;
-	font-size: 14px;
-	width: -webkit-fill-available;
-	overflow: hidden;
-	cursor: text;
-	text-align: center;
-	position: absolute;
-	text-overflow: ellipsis;
-`;
+import { Input, Span } from "./Styled/SearchInput.styled";
+
 export default function SearchInput(props) {
 	const { children } = props;
 	const [searching, setSearching] = useState(false);
