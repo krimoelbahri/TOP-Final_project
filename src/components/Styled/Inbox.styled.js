b07f19a81835e138bd648063ec57fa724a19390a
karim Guezlane <k.guezlane@gmail.com>
2021-10-19 14:47:33 +0100

    adding styles for messages components

--- src/components/Styled/Inbox.styled.js
+++ src/components/Styled/Inbox.styled.js
@@ -33,4 +33,74 @@ export const ConversationContainer = styled.div`
 	height: 100%;
 	width: 100%;
 	overflow: auto;
+	padding-top: 40px;
+	justify-content: flex-end;
 `;
+export const MainConversationContainer = styled.div`
+	height: 100%;
+	width: 100%;
+	flex: 1 1 auto;
+	justify-content: flex-end;
+`;
+// Form Styling to send Messages
+export const SendMessageForm = styled.form`
+	min-height: 40px;
+	display: flex;
+	flex: 0 0 auto;
+	margin: 15px 15px;
+	font-size: large;
+	box-sizing: border-box;
+	border: 1px solid #dbdbdb;
+	border-radius: 20px;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
+export const EmojiDiv = styled.div`
+	flex-direction: row;
+	margin-left: 15px;
+	align-items: center;
+	div {
+		width: 25px;
+		height: 25px;
+		font-size: 25px;
+		margin: 0 15px 0 0;
+		border-radius: 50%;
+	}
+`;
+export const TextArea = styled.textarea`
+	background: 0 0;
+	border: 0;
+	color: #262626;
+	display: flex;
+	flex-grow: 1;
+	font-size: 14px;
+	height: 18px;
+	max-height: 80px;
+	outline: 0;
+	padding: 0;
+	resize: none;
+	width: 0;
+`;
+//styling the component that shows the messages
+
+export const MappingConversationContainer = styled.div`
+	padding: 20px 20px 0;
+	flex: 0 1 auto;
+	overflow-x: hidden;
+	overflow-y: auto;
+`;
+export const MessageContainer = styled.div`
+	flex: 0 0 auto;
+	flex-direction: row;
+	justify-content: flex-end;
+`;
+export const MessageWrapper = styled.div`
+	border: 1px solid #efefef;
+	min-height: 40px;
+	border-radius: 20px;
+	background-color: #efefef;
+	padding: 15px;
+	margin-bottom: 10px;
+`;
+
