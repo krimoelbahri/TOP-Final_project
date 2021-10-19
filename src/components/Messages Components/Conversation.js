8841322dab139ccdc27f295559972c710ce4fa79
karim Guezlane <k.guezlane@gmail.com>
2021-10-19 14:46:12 +0100

    finishing up showing messages in Inbox

--- src/components/Messages Components/Conversation.js
+++ src/components/Messages Components/Conversation.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import { ConversationContainer } from '../Styled/Inbox.styled'
+import React from "react";
+
+import {
+	ConversationContainer,
+	SendMessageForm,
+	TextArea,
+	EmojiDiv,
+	MainConversationContainer,
+} from "../Styled/Inbox.styled";
+import SubmitButton from "../Form Components/SubmitButton";
+import MappingConversation from "./MappingConversation";
 
 export default function Conversation() {
-    return (
-        <ConversationContainer>
-            
-        </ConversationContainer>
-    )
+	return (
+		<ConversationContainer>
+			<MainConversationContainer>
+				<MappingConversation />
+			</MainConversationContainer>
+			<SendMessageForm>
+				<EmojiDiv>
+					<div>
+						<i className='bi bi-emoji-smile'></i>
+					</div>
+				</EmojiDiv>
+				<TextArea placeholder='Your Message... ' />
+				<i className='bi bi-image'></i>
+				<SubmitButton
+					type='submit'
+					fontColor='#0095f6'
+					fontOpacity='.3'
+					disabled='disabled'>
+					<h4>Send</h4>
+				</SubmitButton>
+			</SendMessageForm>
+		</ConversationContainer>
+	);
 }
