872465a722a6adbaacbfead9a2292cfd01e4bf64
karim Guezlane <k.guezlane@gmail.com>
2021-10-22 20:34:41 +0100

    styling profile components

---
+++ src/components/Styled/Profile.styled.js
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	padding-top: 54px;
+	flex: 1;
+	height: 100%;
+	background-color: #fafafa;
+	max-height: 100%;
+`;
+export const ProfilesContainer = styled.div`
+	align-self: center;
+	height: 100%;
+	width: 100%;
+	max-width: 930px;
+	padding: 30px 15px 0 15px;
+`;
+//styling Profile Header section
+export const ProfileHeaderContainer = styled.div`
+	width: 100%;
+	margin-bottom: 40px;
+	flex-direction: row;
+	align-items: center;
+	flex: 0 1 auto;
+`;
+export const ProfilePictureContainer = styled.div`
+	width: 30%;
+	min-width: 200px;
+	height: 100%;
+	align-items: center;
+	justify-content: center;
+	padding: 0 2%;
+	margin-right: 30px;
+	div {
+		height: 150px;
+		width: 150px;
+		img {
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+		}
+	}
+`;
+export const ProfileInfoContainer = styled.div`
+	width: calc(70% - 30px);
+	min-width: 150px;
+	height: 100%;
+	align-items: flex-start;
+	div,
+	ul {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		flex: 1;
+		margin-bottom: 16px;
+		h3,
+		button {
+			margin-right: 15px;
+		}
+		li {
+			margin-right: 30px;
+			font-size: medium;
+			span {
+				font-weight: bold;
+			}
+		}
+	}
+`;
+export const ProfileBio = styled.div`
+	display: block !important;
+	font-size: 16px;
+	line-height: 24px;
+	span {
+		font-weight: bold;
+	}
+`;
+//styling Profile Posts section
+export const ProfilePostsContainer = styled.div`
+	width: 100%;
+`;
+export const PostsHeader = styled.div`
+	width: 100%;
+	height: 50px;
+	border-top: 1px solid #bebebe;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	div {
+		height: 100%;
+		padding: 5px;
+		border-top: 1px solid black;
+		margin-right: 15px;
+		flex-direction: row;
+		align-items: center;
+		i {
+			font-size: 12px;
+		}
+		span {
+			font-size: 12px;
+			font-weight: bold;
+			margin-left: 10px;
+		}
+	}
+`;
+export const PostsContainer = styled.div`
+	width: 100%;
+	flex-direction: row;
+	flex-wrap: wrap;
+	div {
+		width: calc(100% / 3 - 25px);
+		margin-right: 25px;
+		margin-bottom: 25px;
+	}
+`;
