187d7623a13c4de460b2737cb111ed3adb313f45
karim Guezlane <k.guezlane@gmail.com>
2021-10-22 20:34:25 +0100

    showing posts inside Profiles

---
+++ src/components/Profile Components/ProfilePosts.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ProfilePostsContainer, PostsHeader } from "../Styled/Profile.styled";
+import Posts from "./Posts";
+
+export default function ProfilePosts() {
+	return (
+		<ProfilePostsContainer>
+			<PostsHeader>
+				<div>
+					<i className='bi bi-grid-3x3'></i>
+					<span>POSTS</span>
+				</div>
+				<div>
+					<i className='bi bi-bookmark'></i>
+					<span>SAVED</span>
+				</div>
+			</PostsHeader>
+			<Posts />
+		</ProfilePostsContainer>
+	);
+}
