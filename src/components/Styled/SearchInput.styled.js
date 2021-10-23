2613874d82a726f981334d648386bffa8e198e63
karim Guezlane <k.guezlane@gmail.com>
2021-10-23 11:24:13 +0100

    fixing navbar width bug

--- src/components/Styled/SearchInput.styled.js
+++ src/components/Styled/SearchInput.styled.js
@@ -1,24 +1,26 @@
 import styled from "styled-components";
 export const Input = styled.input`
 	background: rgba(250, 250, 250, 1);
+	box-sizing: border-box;
 	outline: 0;
 	overflow: hidden;
-	padding: 5px 5px 5px 26px;
-	font-size: 16px;
+	padding: 4px 4px 4px 26px;
+	font-size: 14px;
 	text-overflow: ellipsis;
 	border: 1px solid #dbdbdb;
 	border-radius: 3px;
-	width: -webkit-fill-available;
+	width: 200px;
 `;
 export const Span = styled.span`
 	background: rgba(250, 250, 250, 1);
 	color: rgba(142, 142, 142, 1);
+	box-sizing: border-box;
 	border: 1px solid #dbdbdb;
 	border-radius: 3px;
-	padding: 7px;
+	padding: 6px 7px;
 	font-family: sans-serif;
 	font-size: 14px;
-	width: -webkit-fill-available;
+	width: 100%;
 	overflow: hidden;
 	cursor: text;
 	text-align: center;
