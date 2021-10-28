cd7ce9530a9c94d7c8fc2a185a8e8150ee27f92e
karim Guezlane <k.guezlane@gmail.com>
2021-10-28 21:07:12 +0100

    styled drop downs

---
+++ src/components/Styled/DropDown.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ActivityContainer = styled.div`
+	width: 400px;
+	min-height: 50px;
+	border-radius: 5px;
+	background-color: #fff;
+`;
+export const ProfileContainer = styled.div`
+	width: 200px;
+	min-height: 50px;
+	border-radius: 5px;
+	background-color: #fff;
+`;
