097e047d98491d481714e7a0083abbcb7ce59f50
karim Guezlane <k.guezlane@gmail.com>
2021-10-28 21:06:56 +0100

    d

--- src/components/Activity.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Container } from "./Styled/Activity.styled";
-
-export default function Activity() {
-	return <Container></Container>;
-}
--- src/components/Styled/Activity.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-	width: 400px;
-	min-height: 50px;
-	border-radius: 5px;
-	background-color: #fff;
-`;
