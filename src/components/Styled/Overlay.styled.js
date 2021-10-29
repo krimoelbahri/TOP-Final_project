366f79a2ab66776a9ace4bf826e68d08f10b53aa
karim Guezlane <k.guezlane@gmail.com>
2021-10-29 14:43:55 +0100

    added animation to overlay

--- src/components/Styled/Overlay.styled.js
+++ src/components/Styled/Overlay.styled.js
@@ -1,7 +1,35 @@
+import { keyframes, css } from "styled-components";
 import styled from "styled-components";
+const fade = keyframes`
+	from {
+    opacity: 0;
+    transform: translate3d(0, -50%, 0);
+  }
 
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
+const fadeOut = keyframes`
+	from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+    transform: translate3d(0, -50%, 0);
+  }
+`;
+const animation = () => css`
+	animation: ${fade} 200ms;
+`;
+const animationOut = () => css`
+	animation: ${fadeOut} 200ms;
+`;
 export const OverlayContainer = styled.div`
 	display: ${({ show }) => (show ? "block" : "none")};
+	${({ show }) => (show ? animation : animationOut)};
 	background: rgba(255, 255, 255, 1);
 	box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.0975);
 	border-radius: 6px;
