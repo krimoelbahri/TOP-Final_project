05c89bd79150c809bbe5358759e15ca8c4c4afc4
karim Guezlane <k.guezlane@gmail.com>
2021-10-30 12:05:24 +0100

    login with facebook and signup auth

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -2,11 +2,30 @@ import React from "react";
 import SignUpForm from "../components/Form Components/SignUpForm";
 import { Container, StyledLink } from "../components/Styled/Auth.styled";
 import { FormConatiner } from "../components/Styled/Form.styled";
+import { useAuth } from "../context/AuthContext";
 
 export default function SignUp() {
+	const { loginWithFacebook, signup } = useAuth();
+	async function handleFacebookLogin() {
+		try {
+			await loginWithFacebook();
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async function handleEmailSignup(email, password) {
+		try {
+			await signup(email, password);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 	return (
 		<Container>
-			<SignUpForm />
+			<SignUpForm
+				handleFacebookLogin={handleFacebookLogin}
+				handleEmailSignup={handleEmailSignup}
+			/>
 			<FormConatiner>
 				<div>
 					<p>
