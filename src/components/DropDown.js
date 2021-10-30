e5c877f5e41f5dfd61109a2e8bd2bfce5bf535bc
karim Guezlane <k.guezlane@gmail.com>
2021-10-30 11:17:23 +0100

    added profile dropdown

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { ActivityContainer, ProfileContainer } from "./Styled/DropDown.styled";
+import {
+	ActivityContainer,
+	ProfileContainer,
+	StyledLink,
+} from "./Styled/DropDown.styled";
 
 export function Activity() {
 	return <ActivityContainer></ActivityContainer>;
 }
-export function Profile() {
-	return <ProfileContainer></ProfileContainer>;
+export function Profile(props) {
+	const { handleLogOut } = props;
+	return (
+		<ProfileContainer>
+			<div>
+				<StyledLink to='/profile'>
+					<i className='bi bi-person-circle'></i>
+					<p>profile</p>
+				</StyledLink>
+			</div>
+			<div onClick={handleLogOut}>
+				<p>Log Out</p>
+			</div>
+		</ProfileContainer>
+	);
 }
