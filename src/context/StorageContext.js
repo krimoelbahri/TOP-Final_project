e1f896f639381eb41678ff89bde889ba342bad83
karim Guezlane <k.guezlane@gmail.com>
2021-10-31 22:12:07 +0100

    created a storage context

---
+++ src/context/StorageContext.js
@@ -0,0 +1,24 @@
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import React, { useContext } from "react";
+
+const StorageContext = React.createContext();
+
+export function useStorage() {
+	return useContext(StorageContext);
+}
+
+export function StorageProvider({ children }) {
+	const storage = getStorage();
+
+	function uploadImages(reference) {
+		return uploadBytes(ref(storage, reference), File);
+	}
+	function DownloadImages(reference) {
+		return getDownloadURL(ref(storage, reference));
+	}
+	const value = { uploadImages, DownloadImages };
+
+	return (
+		<StorageContext.Provider value={value}>{children}</StorageContext.Provider>
+	);
+}
