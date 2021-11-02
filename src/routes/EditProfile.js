02f8a2315ac52801b6fad33f4d7cf51eb15dd487
karim Guezlane <k.guezlane@gmail.com>
2021-11-02 19:24:22 +0100

    reorganizing sub components

--- src/routes/EditProfile.js
+++ src/routes/EditProfile.js
@@ -1,39 +1,22 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import { StyledSubmitButton } from "../components/Styled/Button";
+import EditImage from "../components/Profile Components/EditImage";
+import EditInfo from "../components/Profile Components/EditInfo";
+
 import {
 	Container,
 	MainContainer,
 	EditForm,
-	ProfilePictureContainer,
-	EditImage,
-	UploadImageContainer,
 } from "../components/Styled/EditProfile.styled";
-import { useAuth } from "../context/AuthContext";
-import { useStorage } from "../context/StorageContext";
 
 export default function EditProfile() {
-	const { currentUser } = useAuth();
-
 	return (
 		<Container>
 			<NavBar />
 			<MainContainer>
 				<EditForm>
-					<EditImage>
-						<ProfilePictureContainer>
-							<div>
-								<img src={currentUser.photoURL} alt='PP' />
-							</div>
-						</ProfilePictureContainer>
-						<UploadImageContainer>
-							<h3> {currentUser.displayName} </h3>
-							<StyledSubmitButton style={{ padding: 0 }} fontColor='#0095f6'>
-								Change Profile Photo
-							</StyledSubmitButton>
-							<input style={{ display: "none" }} type='file'></input>
-						</UploadImageContainer>
-					</EditImage>
+					<EditImage />
+					<EditInfo />
 				</EditForm>
 			</MainContainer>
 		</Container>
