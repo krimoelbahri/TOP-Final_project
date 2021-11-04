78de2d046238e7aceee2fc6b3728d575cc4f5abb
karim Guezlane <k.guezlane@gmail.com>
2021-11-04 21:15:58 +0100

    finishing up edit profile info component

--- src/components/Profile Components/EditInfo.js
+++ src/components/Profile Components/EditInfo.js
@@ -1,14 +1,42 @@
 import React from "react";
+import { useAuth } from "../../context/AuthContext";
 import { EditInfoContainer } from "../Styled/EditProfile.styled";
+
 export default function EditInfo() {
+	const { currentUser } = useAuth();
 	return (
 		<EditInfoContainer>
-			<label htmlFor='Name'>
-				<span>
-					<strong>Name</strong>
-				</span>
-				<input type='text' value='Name'></input>
-			</label>
+			<InfoInput type='text' info='Name' value='Name' />
+			<InfoInput type='text' info='Username' value={currentUser.displayName} />
+			<InfoInput type='text' info='Website' value='Website' />
+			<InfoTextarea info='Bio' value='Bio' />
+			<InfoInput type='text' info='Email' value='Email' />
+			<InfoInput type='text' info='Phone Number' value='Phone Number' />
 		</EditInfoContainer>
+	);
+}
+function InfoInput(props) {
+	const { info, type, value, onChange } = props;
+	return (
+		<label htmlFor='Name'>
+			<span>
+				<strong>{info}</strong>
+			</span>
+			<input type={type} defaultValue={value} onChange={onChange}></input>
+		</label>
+	);
+}
+function InfoTextarea(props) {
+	const { info, value, onChange } = props;
+	return (
+		<label htmlFor='Name'>
+			<span>
+				<strong>{info}</strong>
+			</span>
+			<textarea
+				style={{ height: "70px", padding: "5px 10px" }}
+				defaultValue={value}
+				onChange={onChange}></textarea>
+		</label>
 	);
 }
