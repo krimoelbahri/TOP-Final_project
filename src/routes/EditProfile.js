d25e8e06a444da48122f9bc69a6b96dc8a104ad3
karim Guezlane <k.guezlane@gmail.com>
2021-11-05 16:15:37 +0100

    handleing editing User Data

--- src/routes/EditProfile.js
+++ src/routes/EditProfile.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import EditImage from "../components/Profile Components/EditImage";
 import EditInfo from "../components/Profile Components/EditInfo";
-
+import { useData } from "../context/DataContext";
+import { useAuth } from "../context/AuthContext";
+import { useHistory } from "react-router";
 import {
 	Container,
 	MainContainer,
@@ -10,13 +12,38 @@ import {
 } from "../components/Styled/EditProfile.styled";
 
 export default function EditProfile() {
+	//using Custom Hooks
+	const { setData, currentUserData } = useData();
+	const { currentUser, updateProfileNameAndImage } = useAuth();
+	const history = useHistory();
+	//using react Hooks
+	const [editedData, setEditedData] = useState({});
+
+	function handleChange(e) {
+		setEditedData({ ...editedData, [e.target.id]: e.target.value });
+	}
+	async function handleSubmit() {
+		await updateProfileNameAndImage(editedData.Username, currentUser.photoURL);
+		await setData(currentUser.uid, "User", {
+			...currentUserData,
+			...editedData,
+			photoUrl: currentUser.photoURL,
+		});
+		history.push("/profile");
+	}
+
 	return (
 		<Container>
 			<NavBar />
+
 			<MainContainer>
 				<EditForm>
-					<EditImage />
-					<EditInfo />
+					<EditImage data={currentUserData} />
+					<EditInfo
+						data={currentUserData}
+						handleSubmit={handleSubmit}
+						handleChange={handleChange}
+					/>
 				</EditForm>
 			</MainContainer>
 		</Container>
