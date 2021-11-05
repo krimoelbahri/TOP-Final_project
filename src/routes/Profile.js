259d89a94da038d6f10a9c830a2dd7b37b87f0a0
karim Guezlane <k.guezlane@gmail.com>
2021-11-05 16:13:34 +0100

    fetching data to show on profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
 	Container,
 	ProfilesContainer,
 } from "../components/Styled/Profile.styled";
+import { useData } from "../context/DataContext";
+import { useAuth } from "../context/AuthContext";
 import NavBar from "../components/NavBar";
 import ProfileHeader from "../components/Profile Components/ProfileHeader";
 import ProfilePosts from "../components/Profile Components/ProfilePosts";
 
 export default function Profile() {
+	const [loading, setLoading] = useState(true);
+
+	const { getData, setCurrentUserData } = useData();
+	const { currentUser } = useAuth();
+
+	useEffect(() => {
+		console.log("fetching userData");
+		getData(currentUser.uid, "User")
+			.then((result) => {
+				setCurrentUserData(result.data());
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.log(error);
+				setLoading(false);
+			});
+	}, []);
 	return (
 		<Container>
 			<NavBar />
-			<ProfilesContainer>
-                <ProfileHeader/>
-				<ProfilePosts/>
-            </ProfilesContainer>
+			{!loading && (
+				<ProfilesContainer>
+					<ProfileHeader />
+					<ProfilePosts />
+				</ProfilesContainer>
+			)}
 		</Container>
 	);
 }
