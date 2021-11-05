a68f4ee40a1951f88f057400915e3e05c73050ab
karim Guezlane <k.guezlane@gmail.com>
2021-11-05 16:16:52 +0100

    adding default PP

--- src/components/Profile Components/ProfilePicture.js
+++ src/components/Profile Components/ProfilePicture.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { ProfilePictureContainer } from "../Styled/Profile.styled";
 import { useAuth } from "../../context/AuthContext";
+import { useData } from "../../context/DataContext";
 
 export default function PofilePicture() {
 	const { currentUser } = useAuth();
+	const { userPic } = useData();
 
 	return (
 		<ProfilePictureContainer>
 			<div>
-				<img src={currentUser.photoURL} alt='PP' />
+				<img
+					src={currentUser.photoURL ? currentUser.photoURL : userPic}
+					alt='PP'
+				/>
 			</div>
 		</ProfilePictureContainer>
 	);
