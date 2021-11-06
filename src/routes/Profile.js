389e47a00c894c99009486a24dd6abc748129e58
karim Guezlane <k.guezlane@gmail.com>
2021-11-06 13:11:19 +0100

    added loading component to profile header

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import {
 	Container,
+	ProfileHeaderContainer,
 	ProfilesContainer,
 } from "../components/Styled/Profile.styled";
 import { useData } from "../context/DataContext";
 import { useAuth } from "../context/AuthContext";
+import { MyLoader } from "../components/Loaders";
 import NavBar from "../components/NavBar";
 import ProfileHeader from "../components/Profile Components/ProfileHeader";
 import ProfilePosts from "../components/Profile Components/ProfilePosts";
@@ -31,7 +33,13 @@ export default function Profile() {
 		<Container>
 			<NavBar />
 			<ProfilesContainer>
-				{!loading && <ProfileHeader />}
+				{loading ? (
+					<ProfileHeaderContainer>
+						<MyLoader style={{ margin: "0 auto" }} />
+					</ProfileHeaderContainer>
+				) : (
+					<ProfileHeader />
+				)}
 				<ProfilePosts />
 			</ProfilesContainer>
 		</Container>
