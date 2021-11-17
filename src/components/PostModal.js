6cb4473fb73e45655c6bd8247ada1ffd7a30ebf6
karim Guezlane <k.guezlane@gmail.com>
2021-11-17 21:41:56 +0100

    added a component to submit posts

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Modal from "./Modal";
 import { useData } from "../context/DataContext";
+import AddPost from "./AddPost";
 
 const PostModal = () => {
 	const { isModalVisible, setIsModalVisible, toggleBodyOverflosw } = useData();
+
 	function handleClose() {
 		setIsModalVisible(false);
 		toggleBodyOverflosw();
 	}
+
 	return (
-		<Modal isModalVisible={isModalVisible} handleClose={handleClose}></Modal>
+		<Modal isModalVisible={isModalVisible} handleClose={handleClose}>
+			<AddPost />
+		</Modal>
 	);
 };
 export default PostModal;
