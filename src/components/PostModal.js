8899bc95e65b93defedd38c8a0a20b35238ba876
karim Guezlane <k.guezlane@gmail.com>
2021-11-19 16:38:03 +0100

    managing the modal for adding a post

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import { useData } from "../context/DataContext";
 import AddPost from "./AddPost";
 
 const PostModal = () => {
-	const { isModalVisible, setIsModalVisible, toggleBodyOverflosw } = useData();
+	const [display, setDisplay] = useState("none");
+	const [sharing, setSharing] = useState(false);
+	const [image, setImage] = useState("");
+	const { isModalVisible, setIsModalVisible, toggleBodyOverflow } = useData();
 
 	function handleClose() {
-		setIsModalVisible(false);
-		toggleBodyOverflosw();
+		if (image) {
+			let T = global.confirm("do you want to continue");
+			if (T) {
+				setIsModalVisible(false);
+				setImage("");
+				setSharing(false);
+				setDisplay("none");
+				toggleBodyOverflow();
+			}
+		} else {
+			setIsModalVisible(false);
+			toggleBodyOverflow();
+		}
 	}
 
 	return (
 		<Modal isModalVisible={isModalVisible} handleClose={handleClose}>
-			<AddPost />
+			<AddPost
+				setIsModalVisible={setIsModalVisible}
+				display={display}
+				setDisplay={setDisplay}
+				sharing={sharing}
+				setSharing={setSharing}
+				image={image}
+				setImage={setImage}
+			/>
 		</Modal>
 	);
 };
