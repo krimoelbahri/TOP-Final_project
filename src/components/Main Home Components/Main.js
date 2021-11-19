7c6a7e88cfa828afc6927ac3b6c524df886818e7
karim Guezlane <k.guezlane@gmail.com>
2021-11-19 16:36:51 +0100

    mapping through user posts

--- src/components/Main Home Components/Main.js
+++ src/components/Main Home Components/Main.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Post from "./Post";
 import { MainContainer } from "../Styled/MainHome.styled";
+import { usePosts } from "../../context/PostContaxt";
 
 export default function Main() {
+	const { currentUserPosts } = usePosts();
 	return (
 		<MainContainer>
-			<Post />
-			<Post />
-			<Post />
+			{currentUserPosts.posts.map((post, index) => {
+				return <Post key={`post ${index}`} data={post} />;
+			})}
 		</MainContainer>
 	);
 }
