8801d8151ff4cc54fcae62fad5c164bfd2e43d1c
karim Guezlane <k.guezlane@gmail.com>
2021-11-19 16:32:37 +0100

    moved getting data to data context file

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,39 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
 	Container,
 	ProfileHeaderContainer,
 	ProfilesContainer,
 } from "../components/Styled/Profile.styled";
-import { useData } from "../context/DataContext";
-import { useAuth } from "../context/AuthContext";
 import { MyLoader } from "../components/Loaders";
+import { useData } from "../context/DataContext";
 import NavBar from "../components/NavBar";
 import ProfileHeader from "../components/Profile Components/ProfileHeader";
 import ProfilePosts from "../components/Profile Components/ProfilePosts";
 
 export default function Profile() {
-	const [loading, setLoading] = useState(true);
-
-	const { getData, setCurrentUserData } = useData();
-	const { currentUser } = useAuth();
-
-	useEffect(() => {
-		console.log("fetching userData");
-		getData(currentUser.uid, "User")
-			.then((result) => {
-				setCurrentUserData(result.data());
-				setLoading(false);
-			})
-			.catch((error) => {
-				console.log(error);
-				setLoading(false);
-			});
-	}, []);
+	const { profileLoading } = useData();
 	return (
 		<Container>
 			<NavBar />
 			<ProfilesContainer>
-				{loading ? (
+				{profileLoading ? (
 					<ProfileHeaderContainer>
 						<MyLoader style={{ margin: "0 auto" }} />
 					</ProfileHeaderContainer>
