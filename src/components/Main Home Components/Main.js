10f799a1f122bb30a8071d9cb3676ba8c592a837
Karim <k.guezlane@gmail.com>
2021-11-28 09:54:12 +0100

    added a loader to main page

--- src/components/Main Home Components/Main.js
+++ src/components/Main Home Components/Main.js
@@ -2,14 +2,22 @@ import React from "react";
 import Post from "./Post";
 import { MainContainer } from "../Styled/MainHome.styled";
 import { usePosts } from "../../context/PostContaxt";
+import { MyLoader } from "../Loaders";
+import { ProfileHeaderContainer } from "../Styled/Profile.styled";
 
-export default function Main() {
+export default function Main({ loading }) {
 	const { currentUserPosts } = usePosts();
 	return (
 		<MainContainer>
-			{currentUserPosts.posts.map((post, index) => {
-				return <Post key={`post ${index}`} data={post} />;
-			})}
+			{loading ? (
+				<ProfileHeaderContainer>
+					<MyLoader />
+				</ProfileHeaderContainer>
+			) : (
+				currentUserPosts.posts.map((post, index) => {
+					return <Post key={`post ${index}`} data={post} />;
+				})
+			)}
 		</MainContainer>
 	);
 }
