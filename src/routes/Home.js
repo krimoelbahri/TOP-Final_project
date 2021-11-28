844e440bd99b89f5f5836bc6fe32602350897436
Karim <k.guezlane@gmail.com>
2021-11-28 10:20:28 +0100

    cleaning up

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Main from "../components/Main Home Components/Main";
 import { Container } from "../components/Styled/Home.styled";
@@ -7,19 +7,17 @@ import { useAuth } from "../context/AuthContext";
 import { usePosts } from "../context/PostContaxt";
 
 export default function Home() {
-	const { getData, setData, userData } = useData();
+	const { navBarLoading, getData, setData, userData, setNavBarLoading } =
+		useData();
 	const { currentUser, updateProfileNameAndImage } = useAuth();
-	const { postsLoading } = usePosts();
-	const [loading, setLoading] = useState(true);
+	const { postsLoading, setCurrentUserPosts, setPostsLoading } = usePosts();
 	useEffect(() => {
 		if (!currentUser.displayName) {
-			updateProfileNameAndImage("User", "/").then(() => {
-				setLoading(false);
-			});
+			updateProfileNameAndImage("User", "/").then(() => {});
 		}
-		setLoading(false);
 	}, [currentUser.displayName, updateProfileNameAndImage]);
 	useEffect(() => {
+		console.log("user data check");
 		getData(currentUser.uid, "User").then((result) => {
 			if (!result.exists()) {
 				setData(
@@ -32,25 +30,33 @@ export default function Home() {
 						currentUser.email,
 						"",
 						"",
-						""
-					)
+						"",
+					),
 				);
 			}
 		});
-	}, [
-		currentUser.uid,
-		currentUser.displayName,
-		currentUser.photoURL,
-		currentUser.email,
-		getData,
-		setData,
-		userData,
-	]);
+		setNavBarLoading(false);
+	}, [currentUser, getData, setData, userData, setNavBarLoading]);
+	useEffect(() => {
+		console.log("fetching userPosts");
+		getData(currentUser.uid, "Posts")
+			.then((result) => {
+				if (result.exists()) {
+					setCurrentUserPosts(result.data());
+				} else {
+					setCurrentUserPosts({ posts: [] });
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 
+		setPostsLoading(false);
+	}, [getData, setCurrentUserPosts, currentUser, setPostsLoading]);
 	return (
 		<Container>
-			<NavBar loading={loading} />
-			{!postsLoading && <Main loading={loading} />}
+			<NavBar loading={navBarLoading} />
+			<Main loading={postsLoading} />
 		</Container>
 	);
 }
