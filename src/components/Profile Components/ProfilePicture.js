4fd437006aae7ca2da5fe2208d05a6100320b5e8
Karim <k.guezlane@gmail.com>
2021-11-29 11:14:40 +0100

    cleaning

--- src/components/Profile Components/ProfilePicture.js
+++ src/components/Profile Components/ProfilePicture.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { ProfilePictureContainer } from "../Styled/Profile.styled";
 import { useAuth } from "../../context/AuthContext";
-import { useData } from "../../context/DataContext";
 
 export default function PofilePicture() {
 	const { currentUser } = useAuth();
-	const { userPic } = useData();
 
 	return (
 		<ProfilePictureContainer>
 			<div>
-				<img
-					src={currentUser.photoURL ? currentUser.photoURL : userPic}
-					alt='PP'
-				/>
+				<img src={currentUser.photoURL} alt='PP' />
 			</div>
 		</ProfilePictureContainer>
 	);
