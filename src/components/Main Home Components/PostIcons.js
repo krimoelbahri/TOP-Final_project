171ec8d15821526277a6f6ad3b619aac86348f06
Karim <k.guezlane@gmail.com>
2021-11-30 21:15:49 +0100

    handling liking a post

--- src/components/Main Home Components/PostIcons.js
+++ src/components/Main Home Components/PostIcons.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { IconsContainer, IconsWraper } from "../Styled/MainHome.styled";
 
-export default function PostIcons() {
+export default function PostIcons({ handleLikes, fillHeart }) {
 	return (
 		<IconsContainer>
 			<IconsWraper>
 				<div>
-					<i className='far fa-heart'></i>
+					{fillHeart ? (
+						<i
+							onClick={handleLikes}
+							className='fas fa-heart'
+							style={{ color: "red", opacity: " 0.8" }}
+						></i>
+					) : (
+						<i onClick={handleLikes} className='far fa-heart'></i>
+					)}
 				</div>
 				<div>
 					<i className='bi bi-chat-right'></i>
