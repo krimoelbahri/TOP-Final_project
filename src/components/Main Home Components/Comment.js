8337e3ca336f71104b3bbf2dea8ce666f9ac834e
Karim <k.guezlane@gmail.com>
2021-12-01 20:24:02 +0100

    handling showing the comment

--- src/components/Main Home Components/Comment.js
+++ src/components/Main Home Components/Comment.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useData } from "../../context/DataContext";
 import { CommentContainer } from "../Styled/MainHome.styled";
 
-export default function Comment() {
+export default function Comment({ data }) {
+	const [loading, setLoading] = useState(true);
+	const [userInfo, setUserInfo] = useState({ photoUrl: "", Username: "" });
+
+	const { getData } = useData();
+
+	useEffect(() => {
+		getData(data.userId, "User")
+			.then((result) => {
+				setUserInfo(result.data());
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		setLoading(false);
+	}, [data, getData]);
 	return (
 		<CommentContainer>
-			<span>
-				<a href='/'>Profile_Name </a>
-			</span>
-			&nbsp;
-			<span> Some Comments</span>
+			{!loading && (
+				<>
+					<span>
+						{/* TODO: transform this to a link */}
+						<p>{userInfo.Username} </p>
+					</span>
+					&nbsp;
+					<span> {data.comment}</span>
+				</>
+			)}
 		</CommentContainer>
 	);
 }
