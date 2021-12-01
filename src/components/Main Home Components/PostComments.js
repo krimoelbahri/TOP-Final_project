2915dd2aaaf8fd9d7c4cd5b0c5fd1562b2339044
Karim <k.guezlane@gmail.com>
2021-12-01 20:22:52 +0100

    managing comments & somment's age

--- src/components/Main Home Components/PostComments.js
+++ src/components/Main Home Components/PostComments.js
@@ -2,22 +2,41 @@ import React from "react";
 import { ViewCommentsLink, AgeLink } from "../Styled/MainHome.styled";
 import AddComment from "./AddComment";
 import Comment from "./Comment";
+import moment from "moment";
 
-export default function PostComments() {
-	let commentsNumber = 200;
-	let postAge = "1 DAY";
+export default function PostComments({ post, comments, handleSubmitComment }) {
+	function postAge(date) {
+		let diff = Math.abs(new Date() - date.toDate());
+		let age = moment.duration(diff);
+
+		if (age.asDays() >= 1) {
+			return `${age.asDays().toFixed()} Day`;
+		}
+		if (age.asHours() >= 1) {
+			return `${age.asHours().toFixed()} Hour`;
+		}
+		if (age.asMinutes() >= 1) {
+			return `${age.asMinutes().toFixed()} Minute`;
+		}
+		if (age.asSeconds() >= 1) {
+			return `${age.asSeconds().toFixed()} Second`;
+		}
+	}
 	return (
 		<div>
-			<Comment />
-			<Comment />
-			<Comment />
-			<ViewCommentsLink href='/'>
-				<span>View all {commentsNumber} comments</span>
-			</ViewCommentsLink>
+			{comments.map((data, index) => {
+				return <Comment key={`comment${index}`} data={data} />;
+			})}
+
+			{comments.length > 3 && (
+				<ViewCommentsLink>
+					<span>View all {comments.length} comments</span>
+				</ViewCommentsLink>
+			)}
 			<AgeLink>
-				<span>{postAge} AGO</span>
+				<span>{postAge(post.date)} AGO</span>
 			</AgeLink>
-			<AddComment />
+			<AddComment handleSubmitComment={handleSubmitComment} />
 		</div>
 	);
 }
