61bd491fde2be97b784eeb5f874e9b2503996b0f
Karim <k.guezlane@gmail.com>
2021-12-01 20:24:19 +0100

    handling the form to add comments

--- src/components/Main Home Components/AddComment.js
+++ src/components/Main Home Components/AddComment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	CommentForm,
 	EmojiDiv,
@@ -7,20 +7,48 @@ import {
 } from "../Styled/MainHome.styled";
 import SubmitButton from "../Form Components/SubmitButton";
 
-export default function AddComment() {
+export default function AddComment({ handleSubmitComment }) {
+	const [disabled, setDisabled] = useState(true);
+	const [opacity, setOpacity] = useState("0.3");
+	const [comment, setComment] = useState("");
+	const [textValue, setTextValue] = useState("");
+	function handleComment(e) {
+		setTextValue(e.target.value);
+		if (e.target.value) {
+			setDisabled(false);
+			setOpacity("1");
+		}
+		if (!e.target.value) {
+			setDisabled(true);
+			setOpacity("0.3");
+		}
+		setComment(e.target.value);
+	}
+	function handleSubmit(e) {
+		e.preventDefault();
+		handleSubmitComment(comment);
+		setTextValue("");
+		setDisabled(true);
+		setOpacity("0.3");
+	}
 	return (
-		<CommentForm>
+		<CommentForm onSubmit={handleSubmit}>
 			<EmojiDiv>
 				<FormPostIcon>
 					<i className='bi bi-emoji-smile'></i>
 				</FormPostIcon>
 			</EmojiDiv>
-			<TextArea placeholder='Add a comment... ' />
+			<TextArea
+				onChange={handleComment}
+				value={textValue}
+				placeholder='Add a comment... '
+			/>
 			<SubmitButton
 				type='submit'
 				fontColor='#0095f6'
-				fontOpacity='.3'
-				disabled='disabled'>
+				fontOpacity={opacity}
+				disabled={disabled}
+			>
 				<h4>Post</h4>
 			</SubmitButton>
 		</CommentForm>
