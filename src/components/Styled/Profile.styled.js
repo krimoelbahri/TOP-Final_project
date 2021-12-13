7bba8874979e3078599fd810f6a7575028eb8137
Karim <k.guezlane@gmail.com>
2021-12-13 18:55:07 +0100

    deleting container

--- src/components/Styled/Profile.styled.js
+++ src/components/Styled/Profile.styled.js
@@ -1,12 +1,4 @@
 import styled from "styled-components";
-
-export const Container = styled.div`
-	padding-top: 54px;
-	flex: 1;
-	height: 100%;
-	background-color: #fafafa;
-	max-height: 100%;
-`;
 export const ProfilesContainer = styled.div`
 	align-self: center;
 	height: 100%;
