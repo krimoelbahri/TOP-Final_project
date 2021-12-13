d83b1b7eaaba079baed00a004abe0eb1b5e7b57f
Karim <k.guezlane@gmail.com>
2021-12-13 18:53:10 +0100

    styling post image

--- src/components/Main Home Components/PostImage.js
+++ src/components/Main Home Components/PostImage.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 const Container = styled.div`
 	width: 100%;
 	min-height: 300px;
+	max-height: 600px;
 `;
 const Image = styled.img`
 	width: 100%;
 	height: 100%;
-	//max-height: 600px;
 `;
 
 export default function PostImage({ postImgUrl }) {
