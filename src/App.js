b7106b34b0b9180d15656000ba41202903ab51a8
Karim <k.guezlane@gmail.com>
2021-12-13 18:48:28 +0100

    putting context inside one component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from "react";
 import GlobalStyling from "./components/Styled/General.styling";
-import { AuthProvider } from "./context/AuthContext";
-import { DataProvider } from "./context/DataContext";
-import { PostsProvider } from "./context/PostContaxt";
-import { StorageProvider } from "./context/StorageContext";
 import PostModal from "./components/PostModal";
 import "./styles/reset.css";
 import MyRoutes from "./routes/MyRoutes";
+import MyContexts from "./context/MyContexts";
 
 function App() {
 	return (
 		<>
-			<AuthProvider>
-				<StorageProvider>
-					<DataProvider>
-						<PostsProvider>
-							<MyRoutes />
-							<PostModal />
-						</PostsProvider>
-					</DataProvider>
-				</StorageProvider>
-			</AuthProvider>
+			<MyContexts>
+				<MyRoutes />
+				<PostModal />
+			</MyContexts>
 			<GlobalStyling />
 		</>
 	);
