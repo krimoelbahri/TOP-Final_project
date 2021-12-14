e431b8255cd199be3a99e35fc784027b34dd8349
Karim <k.guezlane@gmail.com>
2021-12-14 19:44:47 +0100

    handling showilng profile picture

--- src/components/Profile Components/ProfilePicture.js
+++ src/components/Profile Components/ProfilePicture.js
@@ -2,13 +2,13 @@ import React from "react";
 import { ProfilePictureContainer } from "../Styled/Profile.styled";
 import { useAuth } from "../../context/AuthContext";
 
-export default function PofilePicture() {
+export default function PofilePicture({ profilePic }) {
 	const { currentUser } = useAuth();
 
 	return (
 		<ProfilePictureContainer>
 			<div>
-				<img src={currentUser.photoURL} alt='PP' />
+				<img src={profilePic} alt='PP' />
 			</div>
 		</ProfilePictureContainer>
 	);
