f4bfbfba4044a7321b57dc8b4de9baf1598eb84e
Karim <k.guezlane@gmail.com>
2021-12-14 19:43:05 +0100

    passing props down

--- src/components/Profile Components/ProfileHeader.js
+++ src/components/Profile Components/ProfileHeader.js
@@ -3,11 +3,11 @@ import { ProfileHeaderContainer } from "../Styled/Profile.styled";
 import ProfileInfo from "./ProfileInfo";
 import ProfilePicture from "./ProfilePicture";
 
-export default function ProfileHeader() {
+export default function ProfileHeader({ profilePic, isCurrentUser }) {
 	return (
 		<ProfileHeaderContainer>
-			<ProfilePicture />
-			<ProfileInfo />
+			<ProfilePicture profilePic={profilePic} />
+			<ProfileInfo isCurrentUser={isCurrentUser} />
 		</ProfileHeaderContainer>
 	);
 }
