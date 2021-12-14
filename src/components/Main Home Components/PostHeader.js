1e84380f47879996235ddcfbc23cbd6403743954
Karim <k.guezlane@gmail.com>
2021-12-14 19:42:48 +0100

    linking to post user profile

--- src/components/Main Home Components/PostHeader.js
+++ src/components/Main Home Components/PostHeader.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
 	PostHeaderContainer,
 	ProfileDiv,
 	PostHeaderIcon,
 } from "../Styled/MainHome.styled";
-export default function PostHeader({ userPhoto, userName }) {
+
+export default function PostHeader({ userid, userPhoto, userName }) {
 	return (
 		<PostHeaderContainer>
 			<ProfileDiv>
-				<PostHeaderIcon>
-					<img src={userPhoto} alt='pp' />
-				</PostHeaderIcon>
-				<p>{userName}</p>
+				<Link to={`/profile/${userid}`}>
+					<PostHeaderIcon>
+						{userPhoto && <img src={userPhoto} alt='pp' />}
+					</PostHeaderIcon>
+				</Link>
+				<Link to={`/profile/${userid}`}>
+					<p>{userName}</p>
+				</Link>
 			</ProfileDiv>
 			<div style={{ margin: "0 20px 0 0" }}>
 				<i className='fas fa-ellipsis-h'></i>
