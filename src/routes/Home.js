3ddd26b1ba4ee15bae679a771a5d07ac49f35ad4
Karim <k.guezlane@gmail.com>
2021-12-16 21:22:06 +0100

    cleaning

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from "react";
 import Main from "../components/Main Home Components/Main";
 import { useData } from "../context/DataContext";
 import { useAuth } from "../context/AuthContext";
-import { usePosts } from "../context/PostContaxt";
 
 export default function Home() {
 	const { userPic, getData, setData, userData, setNavBarLoading, following } =
 		useData();
 	const { currentUser, updateProfileNameAndImage } = useAuth();
-	const { postsLoading, setPostsLoading } = usePosts();
+	const [postsLoading, setPostsLoading] = useState(true);
 	const [posts, setPosts] = useState({ posts: [] });
 
 	useEffect(() => {
 		if (!currentUser.displayName) {
-			updateProfileNameAndImage("User", userPic.current).then(() => {});
+			updateProfileNameAndImage("User", userPic.current);
 		}
 	}, [userPic, currentUser, updateProfileNameAndImage]);
+	useEffect(() => {
+		getData("Users", currentUser.uid).then((result) => {
+			if (!result.exists()) {
+				setData("Users", currentUser.uid, { id: currentUser.uid });
+			}
+		});
+	}, [currentUser, getData, setData]);
+
 	useEffect(() => {
 		getData(currentUser.uid, "User").then((result) => {
 			if (!result.exists()) {
@@ -38,32 +45,24 @@ export default function Home() {
 		});
 		setNavBarLoading(false);
 	}, [currentUser, getData, setData, userData, setNavBarLoading]);
-	useEffect(() => {
-		getData("Users", currentUser.uid).then((result) => {
-			if (!result.exists()) {
-				setData("Users", currentUser.uid, { id: currentUser.uid });
-			}
-		});
-	}, [currentUser, getData, setData]);
-	useEffect(() => {
+
+	useEffect(async () => {
 		let arr = [];
-		following.forEach((id) => {
+		await following.forEach((id) => {
 			getData(id, "Posts")
 				.then((result) => {
 					if (result.exists()) {
 						result.data().posts.forEach((post) => arr.push(post));
 						setPosts({ posts: arr });
 					} else {
-						setPosts({ posts: [] });
+						setPosts({ posts: arr });
 					}
 				})
 				.catch((error) => {
 					console.log(error);
 				});
 		});
-
 		setPostsLoading(false);
 	}, [getData, currentUser, setPostsLoading, following]);
-
 	return <Main posts={posts} loading={postsLoading} />;
 }
