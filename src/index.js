b7c1fde7be6b55c4dbaf00dafdc32d7c441f6609
Karim <k.guezlane@gmail.com>
2021-12-17 19:13:48 +0100

    added redux provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
 import "./styles/index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
@@ -7,7 +9,9 @@ import { HashRouter } from "react-router-dom";
 ReactDOM.render(
 	<React.StrictMode>
 		<HashRouter className='App'>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById("root"),
