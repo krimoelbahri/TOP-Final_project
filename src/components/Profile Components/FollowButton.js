eb6cd5e3724a6bd729d5a3201e1def266cfb58e7
Karim <k.guezlane@gmail.com>
2021-12-17 19:16:00 +0100

    added follow button on profiles

---
+++ src/components/Profile Components/FollowButton.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { useData } from "../../context/DataContext";
+import { StyledSubmitButton } from "../Styled/Button";
+import Loader from "react-loader-spinner";
+import { handleFollowing, handleUnFollowing } from "../../Functions/handleFollow";
+import { useDispatch } from "react-redux";
+import { follow, unfollow } from "../../states/followers";
+
+export default function FollowButton() {
+	const [loading, setLoading] = useState(true);
+	const [followButton, setFollowButton] = useState("");
+
+	const { currentUser } = useAuth();
+	const { getData, setData } = useData();
+	const params = useParams();
+	const dispatch = useDispatch();
+
+	const handleClick = async function () {
+		setLoading(true);
+		if (followButton === "Follow") {
+			const obj = await handleFollowing(params.userid, currentUser.uid, getData, setData);
+			setFollowButton("Unfollow");
+			dispatch(follow(obj.following));
+		}
+		if (followButton === "Unfollow") {
+			const obj = await handleUnFollowing(params.userid, currentUser.uid, getData, setData);
+			setFollowButton("Follow");
+			dispatch(unfollow(obj.following));
+		}
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		getData(currentUser.uid, "User")
+			.then((result) => {
+				if (result.data().Following.includes(params.userid)) {
+					setFollowButton("Unfollow");
+				} else {
+					setFollowButton("Follow");
+				}
+
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, [currentUser, params, getData]);
+	return (
+		<>
+			{!loading ? (
+				<StyledSubmitButton
+					onClick={handleClick}
+					backgroundColor='#0095f6'
+					fontColor='white'
+				>
+					{followButton}
+				</StyledSubmitButton>
+			) : (
+				<StyledSubmitButton backgroundColor='#0095f6'>
+					<Loader type='Oval' color='white' height={20} width={50} />
+				</StyledSubmitButton>
+			)}
+		</>
+	);
+}
