8cb40c5357b0c7552825253f410276d917e8f6bb
Karim <k.guezlane@gmail.com>
2021-12-17 19:20:20 +0100

    handeling followers and cleaning

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,53 +1,45 @@
 import React, { useEffect, useState } from "react";
-import {
-	ProfileHeaderContainer,
-	ProfilesContainer,
-} from "../components/Styled/Profile.styled";
+import { ProfileHeaderContainer, ProfilesContainer } from "../components/Styled/Profile.styled";
 import { MyLoader } from "../components/Loaders";
 import { useData } from "../context/DataContext";
 import { useAuth } from "../context/AuthContext";
-import { usePosts } from "../context/PostContaxt";
 import ProfileHeader from "../components/Profile Components/ProfileHeader";
 import ProfilePosts from "../components/Profile Components/ProfilePosts";
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { follow } from "../states/followers";
 
 export default function Profile() {
 	const [isCurrentUser, setIsCurrentUser] = useState(false);
-	const {
-		profileLoading,
-		getData,
-		setCurrentUserData,
-		setEditedData,
-		setProfileLoading,
-		setProfileFollowing,
-		setProfileFollowers,
-		currentUserData,
-	} = useData();
+	const [profileLoading, setProfileLoading] = useState(true);
+	const [postsLoading, setPostsLoading] = useState(true);
+	const [currentUserPosts, setCurrentUserPosts] = useState({ posts: [] });
+	const profileFollowing = useSelector((state) => state.followers.following);
+	//const [profileFollowing, setProfileFollowing] = useState([]);
+	const [profileFollowers, setProfileFollowers] = useState([]);
+
+	const { getData, setCurrentUserData, setEditedData, currentUserData } = useData();
 	const { currentUser } = useAuth();
-	const { setCurrentUserPosts } = usePosts();
 	const params = useParams();
+	const dispatch = useDispatch();
 
 	useEffect(() => {
-		console.log("hi");
-		function fetching() {
-			getData(params.userid, "User")
-				.then((result) => {
-					setCurrentUserData(result.data());
-					if (params.userid === currentUser.uid) {
-						setEditedData(result.data());
-						setIsCurrentUser(true);
-					}
-					setProfileFollowing(result.data().Following);
-					setProfileFollowers(result.data().Followers);
-					setProfileLoading(false);
-				})
-				.catch((error) => {
-					console.log(error);
-					setProfileLoading(false);
-				});
-		}
-		fetching();
-	}, [setEditedData, currentUser, setCurrentUserData, setProfileLoading, params]);
+		getData(params.userid, "User")
+			.then((result) => {
+				setCurrentUserData(result.data());
+				if (params.userid === currentUser.uid) {
+					setEditedData(result.data());
+					setIsCurrentUser(true);
+				}
+				dispatch(follow(result.data().Following));
+				setProfileFollowers(result.data().Followers);
+				setProfileLoading(false);
+			})
+			.catch((error) => {
+				console.log(error);
+				setProfileLoading(false);
+			});
+	}, [setEditedData, currentUser, setCurrentUserData, dispatch, params]);
 	useEffect(() => {
 		getData(params.userid, "Posts")
 			.then((result) => {
@@ -56,6 +48,9 @@ export default function Profile() {
 				} else {
 					setCurrentUserPosts({ posts: [] });
 				}
+			})
+			.then(() => {
+				setPostsLoading(false);
 			})
 			.catch((error) => {
 				console.log(error);
@@ -72,9 +67,12 @@ export default function Profile() {
 				<ProfileHeader
 					profilePic={currentUserData.photoUrl}
 					isCurrentUser={isCurrentUser}
+					posts={currentUserPosts}
+					followers={profileFollowers}
+					following={profileFollowing}
 				/>
 			)}
-			<ProfilePosts />
+			<ProfilePosts loading={postsLoading} posts={currentUserPosts} />
 		</ProfilesContainer>
 	);
 }
