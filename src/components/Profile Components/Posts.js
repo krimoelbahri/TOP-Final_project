ad0a57583a3846a9dc5e3cba0598c025fc6622ba
Karim <k.guezlane@gmail.com>
2021-12-17 19:16:51 +0100

    cleaning

--- src/components/Profile Components/Posts.js
+++ src/components/Profile Components/Posts.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { PostsContainer } from "../Styled/Profile.styled";
-import { usePosts } from "../../context/PostContaxt";
 
-export default function Posts() {
-	const { currentUserPosts } = usePosts();
+export default function Posts({ posts, loading }) {
 	return (
 		<PostsContainer>
-			{currentUserPosts.posts.map((post, index) => {
-				return (
-					<div key={`image ${index}`}>
-						<img src={post.photoUrl} alt='sample-img'></img>
-					</div>
-				);
-			})}
+			{!loading ? (
+				posts.posts.map((post, index) => {
+					return (
+						<div key={`image ${index}`}>
+							<img src={post.photoUrl} alt='sample-img'></img>
+						</div>
+					);
+				})
+			) : (
+				<></>
+			)}
 		</PostsContainer>
 	);
 }
