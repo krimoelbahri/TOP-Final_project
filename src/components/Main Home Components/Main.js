ae4cb55cb80b2492b4ce33907c41846c90b79ab7
Karim <k.guezlane@gmail.com>
2021-12-17 19:15:32 +0100

    handeling showing posts

--- src/components/Main Home Components/Main.js
+++ src/components/Main Home Components/Main.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import { MainContainer } from "../Styled/MainHome.styled";
 import { MyLoader } from "../Loaders";
 import { ProfileHeaderContainer } from "../Styled/Profile.styled";
+import { useData } from "../../context/DataContext";
+import { useAuth } from "../../context/AuthContext";
 
-export default function Main({ loading, posts }) {
+export default function Main() {
+	const { getData } = useData();
+	const { currentUser } = useAuth();
+
+	const [postsLoading, setPostsLoading] = useState(true);
+	const [posts, setPosts] = useState({ posts: [] });
+	const [following, setFollowing] = useState([]);
+	useEffect(() => {
+		getData(currentUser.uid, "User").then((result) => {
+			if (result.exists()) {
+				setFollowing(result.data().Following);
+			}
+		});
+	}, [currentUser, setFollowing]);
+	useEffect(() => {
+		let arr = [];
+		following.forEach((id) => {
+			getData(id, "Posts")
+				.then((result) => {
+					if (result.exists()) {
+						result.data().posts.forEach((post) => arr.push(post));
+						setPosts({ posts: arr });
+					} else {
+						setPosts({ posts: arr });
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		});
+		setPostsLoading(false);
+	}, [getData, currentUser, following]);
 	return (
 		<MainContainer>
-			{loading ? (
+			{postsLoading ? (
 				<ProfileHeaderContainer>
 					<MyLoader />
 				</ProfileHeaderContainer>
