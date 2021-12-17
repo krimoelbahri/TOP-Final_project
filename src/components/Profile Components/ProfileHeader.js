39f4ea471bda0323943ab5a0cd11b58efd2c9906
Karim <k.guezlane@gmail.com>
2021-12-17 19:17:08 +0100

    handeling followers

--- src/components/Profile Components/ProfileHeader.js
+++ src/components/Profile Components/ProfileHeader.js
@@ -3,11 +3,22 @@ import { ProfileHeaderContainer } from "../Styled/Profile.styled";
 import ProfileInfo from "./ProfileInfo";
 import ProfilePicture from "./ProfilePicture";
 
-export default function ProfileHeader({ profilePic, isCurrentUser }) {
+export default function ProfileHeader({
+	followers,
+	following,
+	posts,
+	profilePic,
+	isCurrentUser,
+}) {
 	return (
 		<ProfileHeaderContainer>
 			<ProfilePicture profilePic={profilePic} />
-			<ProfileInfo isCurrentUser={isCurrentUser} />
+			<ProfileInfo
+				followers={followers}
+				following={following}
+				isCurrentUser={isCurrentUser}
+				posts={posts}
+			/>
 		</ProfileHeaderContainer>
 	);
 }
