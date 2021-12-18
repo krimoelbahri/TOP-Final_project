a95f8362aef30954881371aa0181fd21b7a7d8df
Karim <k.guezlane@gmail.com>
2021-12-18 22:13:04 +0100

    filtering and soti,g suggestions

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -6,25 +6,38 @@ import {
 	ProfileIcon,
 	Image,
 	Wrapper,
+	LoaderWrapper,
 } from "./Styled/DropDown.styled";
 import { StyledSubmitButton } from "./Styled/Button";
 import { useData } from "../context/DataContext";
 import { useAuth } from "../context/AuthContext";
+import Loader from "react-loader-spinner";
+import filterSuggestions from "../Functions/arrayHelpers";
 
 export function PeopleSuggestion({ handleFollow }) {
+	const [loading, setloading] = useState(true);
 	const [users, setUsers] = useState([]);
 	const { getDocuments, getData } = useData();
+	const { currentUser } = useAuth();
 
-	useEffect(() => {
-		getDocuments("Users").then((result) => {
-			result.forEach((doc) => {
-				let id = doc.data().id;
-				getData(id, "User").then((result) => {
-					let obj = { ...result.data(), id };
-					setUsers((u) => [...u, obj]);
-				});
+	useEffect(async () => {
+		let result = await getDocuments("Users");
+		result.forEach((doc) => {
+			let id = doc.data().id;
+			if (id === currentUser.uid) return;
+			getData(currentUser.uid, "User").then((result) => {
+				if (result.data().Following.includes(id)) {
+					return;
+				} else {
+					getData(id, "User").then((result) => {
+						let obj = { ...result.data(), id };
+						setUsers((u) => [...u, obj]);
+					});
+				}
 			});
 		});
+
+		setloading(false);
 	}, [getDocuments, getData]);
 
 	return (
@@ -35,24 +48,30 @@ export function PeopleSuggestion({ handleFollow }) {
 					See All
 				</StyledLink>
 			</Wrapper>
-			{users.map((user, index) => {
-				return (
-					<Wrapper key={`suggestion${index}`}>
-						<div>
-							<ProfileIcon>
-								<Image src={user.photoUrl} alt='pp' />
-							</ProfileIcon>
-							<p>{user.Username}</p>
-						</div>
-						<StyledSubmitButton
-							onClick={() => handleFollow(user.id)}
-							fontColor='#0095f6'
-						>
-							Follow
-						</StyledSubmitButton>
-					</Wrapper>
-				);
-			})}
+			{!loading ? (
+				filterSuggestions(users).map((user, index) => {
+					return (
+						<Wrapper key={`suggestion${index}`}>
+							<div>
+								<ProfileIcon>
+									<Image src={user.photoUrl} alt='pp' />
+								</ProfileIcon>
+								<p>{user.Username}</p>
+							</div>
+							<StyledSubmitButton
+								onClick={() => handleFollow(user.id)}
+								fontColor='#0095f6'
+							>
+								Follow
+							</StyledSubmitButton>
+						</Wrapper>
+					);
+				})
+			) : (
+				<LoaderWrapper>
+					<Loader type='Oval' color='black' height={20} width={50} />
+				</LoaderWrapper>
+			)}
 		</PeopleSuggestionContainer>
 	);
 }
