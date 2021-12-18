f29ce20ba69c8fa20c92d2e1a4bb24ef4c11d162
Karim <k.guezlane@gmail.com>
2021-12-18 13:37:30 +0100

    cleaning

--- src/components/Profile Components/ProfileHeader.js
+++ src/components/Profile Components/ProfileHeader.js
@@ -3,16 +3,11 @@ import { ProfileHeaderContainer } from "../Styled/Profile.styled";
 import ProfileInfo from "./ProfileInfo";
 import ProfilePicture from "./ProfilePicture";
 
-export default function ProfileHeader({ followers, following, posts, profilePic, isCurrentUser }) {
+export default function ProfileHeader({ posts, profilePic }) {
 	return (
 		<ProfileHeaderContainer>
 			<ProfilePicture profilePic={profilePic} />
-			<ProfileInfo
-				followers={followers}
-				following={following}
-				isCurrentUser={isCurrentUser}
-				posts={posts}
-			/>
+			<ProfileInfo posts={posts} />
 		</ProfileHeaderContainer>
 	);
 }
