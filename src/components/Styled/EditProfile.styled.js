e221eb42ed618639c2f4528025b888562db9e971
Karim <k.guezlane@gmail.com>
2021-12-19 18:24:05 +0100

    styling span

--- src/components/Styled/EditProfile.styled.js
+++ src/components/Styled/EditProfile.styled.js
@@ -81,3 +81,30 @@ export const ProfilePictureContainer = styled.div`
 		border-radius: 50%;
 	}
 `;
+export const Button = styled.span`
+	background: 0 0;
+	border: 0;
+	box-sizing: border-box;
+	cursor: pointer;
+	font-weight: 600;
+	padding: 5px 9px;
+	text-align: center;
+	width: auto;
+`;
+export const SubmitImageButton = styled(Button)`
+	display: ${(props) => props.display};
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	border-radius: 4px;
+	color: ${(props) => props.fontColor};
+	opacity: ${(props) => props.fontOpacity};
+	position: relative;
+	border: 1px solid transparent;
+	background-color: ${(props) =>
+		props.disabled ? props.disabledBackgroundColor : props.backgroundColor};
+	cursor: ${(props) => (props.disabled ? "default" : "pointer")};
+	div {
+		margin: 0 !important;
+	}
+`;
