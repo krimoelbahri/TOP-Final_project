b8bd783718b26c8a00492e4b18074b9045492b14
Karim <k.guezlane@gmail.com>
2021-12-19 14:15:25 +0100

    styling error component

---
+++ src/components/Styled/Error.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ErrorContainer = styled.div`
+	height: 100%;
+	width: 100%;
+	background-color: #fafafa;
+	max-height: 100%;
+	align-items: center;
+`;
+export const Wrapper = styled.div`
+	height: 40%;
+	width: 50%;
+	align-items: center;
+	justify-content: space-evenly;
+	text-align: center;
+	line-height: 30px;
+	margin-top: 100px;
+`;
