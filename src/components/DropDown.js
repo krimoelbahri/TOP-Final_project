21406efbadba81fa729e067eb94f2b57462af706
Karim <k.guezlane@gmail.com>
2021-12-22 20:38:13 +0100

    styling

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -3,6 +3,7 @@ import {
 	PeopleSuggestionContainer,
 	ProfileContainer,
 	SearchContainer,
+	RemoveContainer,
 	StyledLink,
 	ProfileIcon,
 	Image,
@@ -161,5 +162,24 @@ export function NavBarSearch({ value, handleShowSearch }) {
 				</LoaderWrapper>
 			)}
 		</SearchContainer>
+	);
+}
+export function RemovePost(props) {
+	const { handleShowRemovePost, handleRemovePost, userid } = props;
+	const { currentUser } = useAuth();
+	return (
+		<RemoveContainer>
+			{currentUser.uid === userid && (
+				<div
+					onClick={handleRemovePost}
+					style={{ color: "#ed4956", borderBottom: "1px solid black" }}
+				>
+					<p>Delete</p>
+				</div>
+			)}
+			<div onClick={handleShowRemovePost}>
+				<p>Cancel</p>
+			</div>
+		</RemoveContainer>
 	);
 }
