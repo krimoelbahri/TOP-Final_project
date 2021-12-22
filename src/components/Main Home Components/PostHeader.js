70167970b37633890c161024bccac500f1cb33fb
Karim <k.guezlane@gmail.com>
2021-12-22 20:39:03 +0100

    logic to  remove posts

--- src/components/Main Home Components/PostHeader.js
+++ src/components/Main Home Components/PostHeader.js
@@ -1,8 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { RemovePost } from "../DropDown";
+import Overlay from "../Overlay";
 import { PostHeaderContainer, ProfileDiv, PostHeaderIcon } from "../Styled/MainHome.styled";
+import { useData } from "../../context/DataContext";
+import Loader from "react-loader-spinner";
 
-export default function PostHeader({ userid, userPhoto, userName }) {
+export default function PostHeader({ userid, userPhoto, userName, post }) {
+	const [showRemovePost, setShowRemovePost] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const { getData, setData } = useData();
+
+	const navigate = useNavigate();
+	async function handleRemovePost() {
+		try {
+			setLoading(true);
+			let result = await getData(userid, "Posts");
+			let arr = result.data().posts;
+			arr.splice(post.postId, 1);
+			await setData(userid, "Posts", { posts: arr });
+			navigate(`/profile/${userid}`);
+		} catch (error) {
+			navigate("/error");
+		}
+	}
+
 	return (
 		<PostHeaderContainer>
 			<ProfileDiv>
@@ -13,8 +35,32 @@ export default function PostHeader({ userid, userPhoto, userName }) {
 					<p>{userName}</p>
 				</Link>
 			</ProfileDiv>
-			<div style={{ margin: "0 20px 0 0" }}>
-				<i className='fas fa-ellipsis-h'></i>
+			<div
+				onClick={() => setShowRemovePost(!showRemovePost)}
+				style={{ margin: "0 20px 0 0" }}
+			>
+				{!loading ? (
+					<i className='fas fa-ellipsis-h'></i>
+				) : (
+					<Loader type='Oval' color='black' height={20} width={50} />
+				)}
+
+				<div style={{ marginLeft: "-65px", top: "15px" }}>
+					<Overlay
+						arrowLeft='65px'
+						arrowTop='-6px'
+						show={showRemovePost}
+						handleClick={() => setShowRemovePost(!showRemovePost)}
+					>
+						{showRemovePost && (
+							<RemovePost
+								handleShowRemovePost={() => setShowRemovePost(!showRemovePost)}
+								handleRemovePost={handleRemovePost}
+								userid={userid}
+							/>
+						)}
+					</Overlay>
+				</div>
 			</div>
 		</PostHeaderContainer>
 	);
